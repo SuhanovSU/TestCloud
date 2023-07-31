3aafc4494d6394136d61c8fd569a0334f133656c
SuhanovSU <60311278+SuhanovSU@users.noreply.github.com>
2023-07-31 14:51:05 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,57 +1,11 @@
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
+output "default_instance_public_ip" {
+    value = yandex_compute_instance.default.network_interface[0].nat_ip_address
 }
 
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
+output "subnet_id" {
+    value = data.yandex_vpc_subnet.default_a.subnet_id
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
-
-output "cluster_oidc_issuer_url" {
-  description = "The URL on the EKS cluster OIDC Issuer."
-  value       = module.eks.cluster_oidc_issuer_url
-}
-
-# VPC
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-# Subnets
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
-}
-
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "database_subnets" {
-  description = "List of IDs of database subnets"
-  value       = module.vpc.database_subnets
-}
-
-output "intra_subnets" {
-  description = "List of IDs of intra subnets"
-  value       = module.vpc.intra_subnets
-}
-
-# NAT gateways
-output "nat_public_ips" {
-  description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = module.vpc.nat_public_ips
+output "last_ubuntu" {
+    value = data.yandex_compute_image.last_ubuntu.id
 }
