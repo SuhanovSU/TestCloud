76b442db999cc2541a20b3acb4d2aeed164de861
SuhanovSU <60311278+SuhanovSU@users.noreply.github.com>
2023-07-31 15:17:31 +0300

    add mikritik instance

--- main.tf
+++ main.tf
@@ -19,7 +19,9 @@ provider "yandex" {
 data "yandex_compute_image" "last_ubuntu" {
   family = "ubuntu-2204-lts"  # ОС (Ubuntu, 22.04 LTS)
 }
-
+data "yandex_compute_image" "last_mikrotik" {
+  family = "mikrotik"  # needs to be clarified
+}
 data "yandex_vpc_subnet" "default_a" {
   name = "default-ru-central1-a"  # одна из дефолтных подсетей
 }
@@ -27,19 +29,35 @@ data "yandex_vpc_subnet" "default_a" {
 resource "yandex_compute_instance" "default" { 
   name = "test-instance"
 	platform_id = "standard-v1" # тип процессора (Intel Broadwell)
-
   resources {
     core_fraction = 5 # Гарантированная доля vCPU
     cores  = 2 # vCPU
     memory = 1 # RAM
   }
-
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.last_ubuntu.id
     }
   }
+  network_interface {
+    subnet_id = data.yandex_vpc_subnet.default_a.subnet_id
+    nat = true # автоматически установить динамический ip
+  }
+}
 
+resource "yandex_compute_instance" "default-mk" { 
+  name = "test-mikrotik"
+	platform_id = "standard-v1" # тип процессора (Intel Broadwell)
+  resources {
+    core_fraction = 5 # Гарантированная доля vCPU
+    cores  = 2 # vCPU
+    memory = 1 # RAM
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.last_mikrotik.id
+    }
+  }
   network_interface {
     subnet_id = data.yandex_vpc_subnet.default_a.subnet_id
     nat = true # автоматически установить динамический ip
